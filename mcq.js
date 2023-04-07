0365e30eec6f1eefe5b6a2980bd446d0d1a7f3be
Dheeraj-parashar <127424719+Dheeraj-parashar@users.noreply.github.com>
2023-04-07 15:22:03 +0530

    login page

---
+++ mcq.js
@@ -0,0 +1,25 @@
+const studentData=[{
+    "userId": 'Dheeraj',
+    "password": 'Dhee@1234'
+}
+,
+{
+    "userId": 'Anjali',
+    "password": 'Anjali@1234'
+}]
+const submitButton=document.querySelector("#submit")
+const inputUser=document.querySelector("#userid")
+const pass=document.querySelector("#password")
+
+let access=false;
+let accessor=""
+submitButton.addEventListener("click",()=>{
+    studentData.forEach((e)=>{
+        if((e.userId.toLowerCase())===(inputUser.value.toLowerCase()) && e.password===pass.value){
+            access=true
+            accessor=e.userId
+        }
+    })
+    if(accessor=="") alert("Wrong Credentials")
+    else console.log(accessor)
+})
